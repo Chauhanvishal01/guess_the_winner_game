7fe3dbc0a0655cf41803a962e7094398b8a2f0db
mohitkumar <mohitkumar269856@gmail.com>
2025-04-17 16:02:38 +0530

    update

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-
 const opponentOne = document.getElementById("opponent_1");
 const opponentTwo = document.getElementById("opponent_2");
+const nextBtn = document.getElementById("next-level-btn");
 
-const images = [
+const strong = [
   "../assets/strong/strong1.jpg",
   "../assets/strong/strong2.jpg",
   "../assets/strong/strong3.jpg",
   "../assets/strong/strong4.jpg",
   "../assets/strong/strong5.jpg",
 ];
-const weaks = [
+const weak = [
   "../assets/weak/weak1.jpg",
   "../assets/weak/weak2.jpg",
   "../assets/weak/weak3.jpg",
   "../assets/weak/weak4.jpg",
   "../assets/weak/weak5.jpg",
 ];
+let count = 0;
 
 opponentOne.addEventListener("click", () => {
-  const randomIndex = Math.floor(Math.random() * images.length);
-  opponentOne.src = images[randomIndex];
-  const randomIndex2 = Math.floor(Math.random() * weaks.length);
-  opponentTwo.src = weaks[randomIndex2];
+  const randomIndex = Math.floor(Math.random() * strong.length);
+  opponentOne.src = strong[randomIndex];
+  const randomIndex2 = Math.floor(Math.random() * weak.length);
+  opponentTwo.src = weak[randomIndex2];
+  count += 1;
 });
 opponentTwo.addEventListener("click", () => {
-  const randomIndex = Math.floor(Math.random() * images.length);
-  opponentTwo.src = images[randomIndex];
-  const randomIndex2 = Math.floor(Math.random() * weaks.length);
-  opponentOne.src = weaks[randomIndex2];
+  const randomIndex = Math.floor(Math.random() * strong.length);
+  opponentTwo.src = strong[randomIndex];
+  const randomIndex2 = Math.floor(Math.random() * weak.length);
+  opponentOne.src = weak[randomIndex2];
+  count += 1;
 });
+console.log(count);

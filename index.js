1a73a39d28430d0a928f3fb85e6321e014ceee25
Vishal Chauhan <vishal2825m@gmail.com>
2025-04-17 15:37:14 +0530

    update js

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-/* let score = 0;
-let right, wrong;
-let start;
 
-function isRight(right) {
-  if (right) {
-    score += 1;
-  }
-}
-
-function isRestart(start) {
-  if (start) {
-    score = 0;
-  }
-}
- */
 const opponentOne = document.getElementById("opponent_1");
 const opponentTwo = document.getElementById("opponent_2");
 
-console.log(opponentOne);
-console.log(opponentTwo);
+const images = [
+  "../assets/strong/strong1.jpg",
+  "../assets/strong/strong2.jpg",
+  "../assets/strong/strong3.jpg",
+  "../assets/strong/strong4.jpg",
+  "../assets/strong/strong5.jpg",
+];
+const weaks = [
+  "../assets/weak/weak1.jpg",
+  "../assets/weak/weak2.jpg",
+  "../assets/weak/weak3.jpg",
+  "../assets/weak/weak4.jpg",
+  "../assets/weak/weak5.jpg",
+];
+
+opponentOne.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  opponentOne.src = images[randomIndex];
+  const randomIndex2 = Math.floor(Math.random() * weaks.length);
+  opponentTwo.src = weaks[randomIndex2];
+});
+opponentTwo.addEventListener("click", () => {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  opponentTwo.src = images[randomIndex];
+  const randomIndex2 = Math.floor(Math.random() * weaks.length);
+  opponentOne.src = weaks[randomIndex2];
+});
